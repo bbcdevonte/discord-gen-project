ab7588aabdabc4deda73918d8fda3c01cfbcaf4d
bbcdevonte <100002197+bbcdevonte@users.noreply.github.com>
2022-02-18 17:15:24 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "Bot Token Here!",
+    token: "OTQ0MzY4NzIyNjIwMDcyMDQ2.YhAl6A.2i_3qheRvUQhXLhGeU9SCLRs7d0",
     prefix: "/gen ",
     discord_server: "https://discord.gg/irylek"
 }
